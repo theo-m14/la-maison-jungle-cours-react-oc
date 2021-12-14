c433358dd545eab25bcc6b221a6b978999e898e2
Michel Théo <theo.michel2@outlook.fr>
2021-12-14 17:22:00 +0100

    Exo prop ( css from solution )

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import { plantList } from '../datas/plantList'
 import '../styles/ShoppingList.css'
+import PlantItem from './PlantItem'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -16,11 +17,14 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-                        {plant.isSpecialOffer && <div className='lmj-sales'>PROMOTION</div>}
-					</li>
+				{plantList.map(({id, name, light, water,cover}) => (
+					<PlantItem 
+                        name={name}
+                        cover={cover}
+                        light={light}
+                        water={water}
+                        id={id}
+                    />
 				))}
 			</ul>
 		</div>
