55d6ac58130fac4bde66c288f6d798b3ba86fc9a
Michel Théo <theo.michel2@outlook.fr>
2021-12-15 13:47:11 +0100

    adding btn for delete item-cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -22,6 +22,7 @@ function Cart({ cart, updateCart }) {
 						{cart.map(({ name, price, amount }, index) => (
 							<div key={`${name}-${index}`}>
 								{name} {price}€ x {amount}
+                <button onClick={() => deleteItemCart(name,price)}>X</button>
 							</div>
 						))}
 					</ul>
@@ -42,6 +43,24 @@ function Cart({ cart, updateCart }) {
 			</button>
 		</div>
 	)
+
+  function deleteItemCart(name,price){
+    //On regarde si la cart contient bien la plante
+    const currentPlant = cart.find((plant) => plant.name === name);
+    //Si jamais on a bien une plante qui porte ce nom alors
+    if(currentPlant){
+      //Je récupère la plante en question
+      const cartFilteredCurrentPlant = cart.filter((plant)=> plant.name !== name);
+      //Puis j'update sa quantité
+      updateCart([...cartFilteredCurrentPlant, { name, price, amount: currentPlant.amount - 1 }])
+      //Je vérifie ici si la quantité est égale à 1, car updateCart n'a pas encore retiré le montant voulu, je regarde l'état d'avant
+      if(currentPlant.amount===1){
+        updateCart(cart.filter((plant)=> plant.amount!==1));
+      }
+    }
+  }
 }
+
+
 
 export default Cart
