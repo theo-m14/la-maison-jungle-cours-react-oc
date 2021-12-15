c6cfee0b19608d2fe072864e9d07ff00dc7806d0
Michel Théo <theo.michel2@outlook.fr>
2021-12-15 11:46:57 +0100

    update app to exercice base

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
+import { useState } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
-import ShoppingList from './ShoppingList'
-import QuestionForm from './QuestionForm'
-import Footer from './Footer'
 import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
+	const [cart, updateCart] = useState([])
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			<Cart />
-			<ShoppingList />
-			<QuestionForm />
-			<Footer/>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
 		</div>
 	)
 }
